69ee3efb993430a6f3238ed37e84c8e176233c0b
Tuur-Van-Bergen <146072364+Tuur-Van-Bergen@users.noreply.github.com>
2024-02-07 14:37:32 +0100

    Create 01. My First Project.py

---
+++ Hyperskill Projects/Python Projects/01. My First Project.py
@@ -0,0 +1,21 @@
+print("Earned amount:")
+bubblegum = 202
+toffee = 118
+ice_cream = 2250
+milk_chocolate = 1680
+doughnut = 1075
+pancake = 80
+print(f"Bubblegum: ${bubblegum}")
+print(f"Toffee: ${toffee}")
+print(f"Ice cream: ${ice_cream}")
+print(f"Milk chocolate: ${milk_chocolate}")
+print(f"Doughnut: ${doughnut}")
+print(f"Pancake: ${pancake}")
+income = bubblegum + toffee + ice_cream + milk_chocolate + doughnut + pancake
+print(f"Income: ${income}")
+print("Staff expenses:")
+x = int(input())
+print("Other expenses:")
+y = int(input())
+net_income = income - x - y
+print(f"Net income: ${net_income}")
